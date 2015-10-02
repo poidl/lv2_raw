626087b1fc574af8d6616a9184423743ce44604d
poidl <hoitaus@gmail.com>
2015-10-02 13:39:21 +1000

    simple oscillator responding to midi input

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,222 @@
 extern crate libc;
 use std::ptr;
 use std::mem;
+use std::str;
+use std::ffi::CString;
+use std::ffi::CStr;
+use std::io::Write;
+
 mod lv2;
 
-pub enum PortIndex {
-        MIDI_IN = 0,
-        AUDIO_OUT = 1
+macro_rules! println_stderr(
+    ($($arg:tt)*) => (
+        match writeln!(&mut ::std::io::stderr(), $($arg)* ) {
+            Ok(_) => {},
+            Err(x) => panic!("Unable to write to stderr: {}", x),
+        }
+    )
+);
+
+enum PortIndex {
+    MidiIn = 0,
+    AudioOut = 1
+}
+
+// Unnecessary? See comment in connect_port().
+impl PortIndex {
+    fn from_int(x: u32) -> PortIndex {
+        match x {
+            0 => PortIndex::MidiIn,
+            1 => PortIndex::AudioOut,
+            _ => panic!("Not a valid PortIndex: {}", x)
+        }
+    }
 }
 
 #[repr(C)]
 struct Synth {
-    gain: *const f32,
-    input: *const f32,
-    output: *mut f32
+    map: *const lv2::Lv2UridMap,
+    in_port: *const lv2::LV2_Atom_Sequence,
+    output: *mut f32,
+    uris: SynthURIs,
+    rate: f64,
+    currentfreq: f64,
+    currentmidivel: u8,
+    waveoffset: u32,
+    noteison: bool,
+    makesilence: bool,
+    osc: Osc
 }
 
-
 impl lv2::LV2Descriptor {
-    pub extern fn instantiate(_descriptor: *const lv2::LV2Descriptor, _rate: &mut f64, _bundle_path: *const u8, _features: *const lv2::LV2Feature)
-                                -> lv2::Lv2handle {
-                                let ptr: *mut libc::c_void;
-                                unsafe{
-                                    ptr = libc::malloc(mem::size_of::<Synth>() as libc::size_t) as *mut libc::c_void;
-                                }
-                                return ptr;
+    pub extern fn instantiate( _descriptor: *const lv2::LV2Descriptor , rate: f64, bundle_path: *const libc::c_char , features: *const (*const lv2::LV2Feature),) -> lv2::Lv2handle {
+        unsafe{
+        let mut ptr = libc::calloc(1,mem::size_of::<Synth>() as libc::size_t);
+        if ptr.is_null() {
+            panic!("failed to allocate memory");
+        }
+
+        let mut map = (*(ptr  as *mut Synth)).map;
+
+        let uridmapstr = "http://lv2plug.in/ns/ext/urid#map";
+        let mut x: isize = 0;
+        let mut done = false;
+        while !done {
+
+                let fptr: *const lv2::LV2Feature = *features.offset(x);
+                if fptr.is_null() {
+                    // host doesn't provide feature
+                    libc::free(ptr as *mut libc::c_void);
+                    //let mut ptr: *mut libc::c_void = std::ptr::null_mut();
+                    println!("Missing feature \"{}\"", uridmapstr);
+                    //return ptr
+                    return std::ptr::null_mut();
+                }
+                let uriptr = (*fptr).URI;
+                let buf = CStr::from_ptr(uriptr).to_bytes();
+                let s: &str = str::from_utf8(buf).unwrap();
+                println!("uri: {}", s);
+                if s == uridmapstr {
+
+                    map = (*fptr).data;
+                    done=true;
+                    println!{" -> obtained urid#map from host"}
+                }
+
+            x = x+1;
+        }
+
+        let uris_addr = &mut (*(ptr  as *mut Synth)).uris;
+        map_synth_uris(map, uris_addr);
+
+        (*(ptr  as *mut Synth)).rate = rate;
+        (*(ptr  as *mut Synth)).noteison = false;
+        (*(ptr  as *mut Synth)).makesilence = false;
+        (*(ptr  as *mut Synth)).osc = Osc { phase: 0, dphase: 0 };
+        unsafe{
+            (*(ptr  as *mut Synth)).osc.set_dphase(440.0,(*(ptr  as *mut Synth)).rate);
+            println!("self.dphase: {}",(*(ptr  as *mut Synth)).osc.dphase);
+        }
+        return ptr;
+        }
     }
-    pub extern fn connect_port(handle: lv2::Lv2handle, port: u32, data: *mut libc::c_void) {}
+
+    pub extern fn connect_port(handle: lv2::Lv2handle, port: u32, data: *mut libc::c_void) {
+        let synth: *mut Synth = handle as *mut Synth;
+        // simpler to use PortIndex instead of u32 for port, but that doesn't correspond to C?
+        match PortIndex::from_int(port) {
+            // data may be NULL pointer -> don't dereference!
+        // match port {
+            PortIndex::MidiIn => unsafe{ (*synth).in_port = data  as *const lv2::LV2_Atom_Sequence},
+            PortIndex::AudioOut => unsafe{ (*synth).output = data as *mut f32 },
+        }
+
+    }
     pub extern fn activate(_instance: lv2::Lv2handle) {}
-    pub extern fn run(instance: lv2::Lv2handle, n_samples: u32) {}
+
+    pub extern fn run(instance: lv2::Lv2handle, n_samples: u32) {
+        unsafe{
+            let synth = instance as *mut Synth;
+            let uris = &mut (*synth).uris;
+            // Struct for a 3 byte MIDI event, used for writing notes
+            struct MIDINoteEvent {
+                event: lv2::LV2_Atom_Event,
+                msg: [u8; 3]
+            }
+            // Initially self->out_port contains a Chunk with size set to capacity
+            // Get the capacity
+            //let out_capacity: u32 = (*synth).out_port->atom.size;
+            let seq = (*synth).in_port;
+            let output = (*synth).output;
+            let mut iter: *const lv2::LV2_Atom_Event  = lv2::lv2_atom_sequence_begin(&(*seq).body);
+            while !lv2::lv2_atom_sequence_is_end(&(*seq).body, (*seq).atom.size, iter) {
+                println!("next");
+                let ev = iter;
+                println!("(*ev).body.mytype: {}", (*ev).body.mytype);
+                if (*ev).body.mytype == (*uris).midi_event {
+                    println!("**********ISMIDI**********");
+                    let msg: *const u8 = ev.offset(1) as *const u8;
+                    println!("message:     = {0:x}", *msg);
+                    // let isvoice = lv2::lv2_midi_is_voice_message(msg);
+                    // println!("message is voice:     = {}", isvoice);
+                    let istart = (*ev).time_in_frames as u32;
+                    match lv2::lv2_midi_message_type(msg) {
+                        lv2::LV2_Midi_Message_Type::LV2_MIDI_MSG_NOTE_ON => {
+                            println!("NOTEON AT FRAME {}", istart);
+                            (*synth).noteison = true;
+                            (*synth).waveoffset = n_samples-istart;
+                            let freq = freq_from_midimsg(msg);
+                            (*synth).currentfreq = freq;
+                            (*synth).currentmidivel = *msg.offset(2);
+                            //let amp = amp(isample, freq, (*synth).rate);
+                            let coef = 1.0 as f32;
+
+                            (*synth).osc.reset();
+                            (*synth).osc.set_dphase(freq,(*synth).rate);
+
+                            for i in istart..n_samples-1 {
+
+                                //let amp = amp(i-istart, freq, (*synth).rate);
+                                let amp = (*synth).osc.get();
+                                println!("................writing to output amp {}", amp);
+                                *output.offset(i as isize) = amp;
+                            }
+                            println!("END OF RUN CHUNK")
+
+                        }
+
+
+                        lv2::LV2_Midi_Message_Type::LV2_MIDI_MSG_NOTE_OFF => {
+                            println!("NOTEOFFFF AT FRAME {}", istart);
+                            (*synth).noteison = false;
+                            (*synth).makesilence = true;
+                            for i in istart..n_samples-1 {
+                                let amp = 0.0 as f32;
+                                println!("noteoff silence: output amp {}", amp);
+                                *output.offset(i as isize) = (amp as f32);
+                            }
+
+
+                        }
+
+                        _ => {
+                            println!("DON'T UNDERSTAND MESSAGE")
+                        }
+
+                    }
+                }
+                //         const uint8_t* const msg = (const uint8_t*)(ev + 1);
+                //         switch (lv2_midi_message_type(msg))
+                iter = lv2::lv2_atom_sequence_next(iter);
+            }
+
+            if (*synth).noteison {
+                let coef = 1.0 as f32;
+                let offs = (*synth).waveoffset;
+                let freq = (*synth).currentfreq;
+
+                for i in 0..n_samples-1 {
+                    //let amp = amp(i+offs-1, freq, (*synth).rate);
+                    let amp = (*synth).osc.get();
+                    println!("keep playing output amp {}", amp);
+                    *output.offset(i as isize) = (amp as f32) * coef;
+                }
+                (*synth).waveoffset = n_samples-offs;
+                println!("END OF RUN CHUNK")
+
+            } else if (*synth).makesilence {
+                (*synth).makesilence = false;
+                for i in 0..n_samples-1 {
+                    let amp = 0.0;
+                    println!("keep silence output amp {}", amp);
+                    *output.offset(i as isize) = amp as f32;
+                }
+                println!("END OF RUN CHUNK")
+            }
+
+        }
+
+    }
 
     pub extern fn deactivate(_instance: lv2::Lv2handle) {}
     pub extern fn cleanup(instance: lv2::Lv2handle) {
@@ -42,9 +231,9 @@ impl lv2::LV2Descriptor {
     }
 }
 
-static S: &'static [u8] = b"http://example.org/eg-amp_rust\0";
+static S: &'static [u8] = b"http://example.org/brrrsynth\0";
 static mut desc: lv2::LV2Descriptor = lv2::LV2Descriptor {
-    amp_uri: 0 as *const libc::c_char, // ptr::null() isn't const fn (yet)
+    uri: 0 as *const libc::c_char, // ptr::null() isn't const fn (yet)
     instantiate: lv2::LV2Descriptor::instantiate,
     connect_port: lv2::LV2Descriptor::connect_port,
     activate: lv2::LV2Descriptor::activate,
@@ -62,8 +251,75 @@ pub extern fn lv2_descriptor(index:i32) -> *const lv2::LV2Descriptor {
         // credits to ker on stackoverflow: http://stackoverflow.com/questions/31334356/static-struct-with-c-strings-for-lv2-plugin (duplicate) or http://stackoverflow.com/questions/25880043/creating-a-static-c-struct-containing-strings
         let ptr = S.as_ptr() as *const libc::c_char;
         unsafe {
-        desc.amp_uri = ptr;
+        desc.uri = ptr;
         return &desc as *const lv2::LV2Descriptor
         }
+    }
+}
+
+struct SynthURIs {
+    midi_event: lv2::Lv2Urid
+}
+
+fn map_synth_uris(map: *const lv2::Lv2UridMap, uris: &mut SynthURIs) {
+    let s = "http://lv2plug.in/ns/ext/midi#MidiEvent";
+    let cstr = CString::new(s).unwrap();
+    let lv2_midi_midi_event = cstr.as_ptr();
+    //mem::forget(cstr);
+    unsafe{
+        uris.midi_event = ((*map).map)((*map).handle, lv2_midi_midi_event);
+    }
+}
+
+fn freq_from_midimsg(msg: *const u8) -> f64 {
+    // A3 has midi number 56
+    // Frequencies are calculated with the formula
+    // freq = {[(2)^1/12]^n} * 220 Hz,
+    // where n is the number of half steps from A3
+    unsafe{
+        let i = *msg.offset(1);
+        println!("**************************** i: {}",i);
+        let freq = (2.0f64.powf((((i as i8)-57) as f64)/12.0))*220.0;
+        println!("FFFFFFFFFFFFFFFFFFFFFFFREQ: {}", freq);
+        return freq
+    }
+}
+
+fn amp(isample: u32, freq: f64, rate: f64) -> f64 {
+    let lam = rate/freq;
+    println!("rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrate: {}", rate);
+    return (2.0*std::f64::consts::PI*((isample as f64)/lam)).sin()
+}
+
+struct Osc {
+    phase: u32,
+    dphase: u32
+}
+
+impl Osc {
+    fn reset(& mut self) {
+        self.phase =  0
+    }
+    fn set_dphase(&mut self, freq: f64, rate: f64) {
+		// Phase increment of the phase accumulator. (freq/rate) is the
+        // fraction of period per sample. This is multiplied by 2^32, so
+        // each frequency is equivalent to a fraction of the "maximum
+        // phase increment" 2^32, which corresponds to  freq = rate.
+		// (2^32)/16=268435456
+        self.dphase =  ((freq/rate)*4294967296.0) as u32;
+        //println!("bla: {}",freq*(0xFFFFFFFF as u32))
+    }
+    fn step(&mut self){
+        //let x = Wrapping(self.phase);
+        //let y = Wrapping(self.dphase);
+        //self.phase = (x+y).0;
+        // wrapping_add: allows intentional overflow
+        self.phase = self.phase.wrapping_add(self.dphase);
+        println!("self.phase: {}", self.phase);
+    }
+    fn get(&mut self) -> f32 {
+        self.step();
+        let phi: f32 = (self.phase as f64/4294967296.0) as f32;
+        return phi
     }
 }
--- src/lv2.rs
+++ src/lv2.rs
@@ -1,49 +1,93 @@
 extern crate libc;
+use std::mem;
 
 pub type Lv2handle = *mut libc::c_void;
 
 // The header of an atom:Atom.
 #[repr(C)]
 pub struct LV2_Atom {
-	size: u32,  // Size in bytes, not including type and size.
-	mytype: u32  // Type of this atom (mapped URI).
+	pub size: u32,  // Size in bytes, not including type and size.
+	pub mytype: u32  // Type of this atom (mapped URI).
+}
+
+// compare with
+//http://lv2plug.in/git/cgit.cgi/lv2.git/tree/lv2/lv2plug.in/ns/ext/atom/atom.h
+// LV2_Atom_Event has a union "time", which can be beat or frames. Not implemented
+// doesn't need #[repr(C)]
+pub struct LV2_Atom_Event {
+	pub time_in_frames: i64,
+	pub body: LV2_Atom
 }
 
 #[repr(C)]
 pub struct LV2_Atom_Sequence_Body {
 	unit: u32,  // URID of unit of event time stamps.
 	pad: u32   // Currently unused.
+	/* Contents (a series of events) follow here. */
 }
 
 // An atom:Sequence.
 #[repr(C)]
 pub struct LV2_Atom_Sequence {
-	atom: LV2_Atom, // Atom header.
-	body: LV2_Atom_Sequence_Body  // Body.
+	pub atom: LV2_Atom, // Atom header.
+	pub body: LV2_Atom_Sequence_Body  // Body.
 }
 
-pub type LV2_URID_Map_Handle = *mut libc::c_void;
-pub type LV2_URID = u32;
+/** Pad a size to 64 bits. */
+pub fn lv2_atom_pad_size(size: u32) -> (u32) {
+	// println!("7u32     = {:0>32b}", 7u32);
+	// println!("!7u32     = {:0>32b}", !7u32);
+	return (size + 7u32) & (!7u32)
+}
+
+/** Get an iterator pointing to the first event in a Sequence body. */
+pub fn lv2_atom_sequence_begin(body: *const LV2_Atom_Sequence_Body) ->  (*const LV2_Atom_Event) {
+	unsafe{
+		return body.offset(1) as *const LV2_Atom_Event
+	}
+}
+
+/** Return an iterator to the element following `i`. */
+pub fn lv2_atom_sequence_next(i: *const LV2_Atom_Event) -> (*const LV2_Atom_Event)
+{
+	unsafe{
+		let addr_of_first_byte = i as *const u8;
+		let size_in_bytes_1 = mem::size_of::<LV2_Atom_Event>() as isize;
+		let size_in_bytes_2 = lv2_atom_pad_size((*i).body.size) as isize;
+		let j = addr_of_first_byte.offset(size_in_bytes_1 + size_in_bytes_2);
+		return j as *const LV2_Atom_Event
+	}
+}
+
+/** Return true iff `i` has reached the end of `body`. */
+pub fn lv2_atom_sequence_is_end(body: *const LV2_Atom_Sequence_Body, size: u32, i: *const LV2_Atom_Event) -> (bool) {
+	let addr_of_first_byte = body as *const u8;
+	unsafe{
+		return (i as *const u8) >= addr_of_first_byte.offset(size as isize)
+	}
+}
+
+pub type Lv2UridMapHandle = *mut libc::c_void;
+pub type Lv2Urid = u32;
 
 #[repr(C)]
-pub struct LV2_URID_Map {
-	handle: LV2_URID_Map_Handle, // Opaque pointer to host data.
+pub struct Lv2UridMap {
+	pub handle: Lv2UridMapHandle, // Opaque pointer to host data.
 	   //@param handle Must be the callback_data member of this struct.
 	   //@param uri The URI to be mapped to an integer ID.
-	map: extern fn(handle: LV2_URID_Map_Handle, uri: *const libc::c_char)-> LV2_URID
+	pub map: extern fn(handle: Lv2UridMapHandle, uri: *const libc::c_char)-> Lv2Urid
 }
 
 #[repr(C)]
 pub struct LV2Feature {
-    uri: *const u8,
-    data: *mut libc::c_void
+    pub URI: *const libc::c_char,
+    pub data: *mut Lv2UridMap
 }
 
 #[repr(C)]
 pub struct LV2Descriptor {
-    pub amp_uri: *const  libc::c_char,
-    pub instantiate: extern fn(descriptor: *const LV2Descriptor, rate:  &mut f64,
-                            bundle_path: *const u8, features: *const LV2Feature)
+    pub uri: *const  libc::c_char,
+    pub instantiate: extern fn(descriptor: *const LV2Descriptor,rate: f64, bundle_path: *const libc::c_char, 	features: *const (*const LV2Feature) )
                                 -> Lv2handle,
     pub connect_port: extern fn(handle: Lv2handle, port: u32, data: *mut libc::c_void),
     pub activate: extern fn(instance: Lv2handle),
@@ -51,4 +95,70 @@ pub struct LV2Descriptor {
     pub deactivate: extern fn(instance: Lv2handle),
     pub cleanup: extern fn(instance: Lv2handle),
     pub extension_data: extern fn(uri: *const u8)-> (*const libc::c_void),
+}
+
+// typedef enum {
+// 	LV2_MIDI_MSG_INVALID          = 0,     /**< Invalid Message */
+// 	LV2_MIDI_MSG_NOTE_OFF         = 0x80,  /**< Note Off */
+// 	LV2_MIDI_MSG_NOTE_ON          = 0x90,  /**< Note On */
+// 	LV2_MIDI_MSG_NOTE_PRESSURE    = 0xA0,  /**< Note Pressure */
+// 	LV2_MIDI_MSG_CONTROLLER       = 0xB0,  /**< Controller */
+// 	LV2_MIDI_MSG_PGM_CHANGE       = 0xC0,  /**< Program Change */
+// 	LV2_MIDI_MSG_CHANNEL_PRESSURE = 0xD0,  /**< Channel Pressure */
+// 	LV2_MIDI_MSG_BENDER           = 0xE0,  /**< Pitch Bender */
+// 	LV2_MIDI_MSG_SYSTEM_EXCLUSIVE = 0xF0,  /**< System Exclusive Begin */
+// 	LV2_MIDI_MSG_MTC_QUARTER      = 0xF1,  /**< MTC Quarter Frame */
+// 	LV2_MIDI_MSG_SONG_POS         = 0xF2,  /**< Song Position */
+// 	LV2_MIDI_MSG_SONG_SELECT      = 0xF3,  /**< Song Select */
+// 	LV2_MIDI_MSG_TUNE_REQUEST     = 0xF6,  /**< Tune Request */
+// 	LV2_MIDI_MSG_CLOCK            = 0xF8,  /**< Clock */
+// 	LV2_MIDI_MSG_START            = 0xFA,  /**< Start */
+// 	LV2_MIDI_MSG_CONTINUE         = 0xFB,  /**< Continue */
+// 	LV2_MIDI_MSG_STOP             = 0xFC,  /**< Stop */
+// 	LV2_MIDI_MSG_ACTIVE_SENSE     = 0xFE,  /**< Active Sensing */
+// 	LV2_MIDI_MSG_RESET            = 0xFF   /**< Reset */
+// } LV2_Midi_Message_Type;
+
+/**
+   Return true iff `msg` is a MIDI voice message (which has a channel).
+*/
+pub fn lv2_midi_is_voice_message(msg: *const u8) -> (bool) {
+	unsafe{
+		return (*msg) >= 0x80 && (*msg) < 0xF0;
+	}
+}
+
+/**
+   Return the type of a MIDI message.
+   @param msg Pointer to the start (status byte) of a MIDI message.
+*/
+pub fn lv2_midi_message_type(msg: *const u8) -> (LV2_Midi_Message_Type) {
+	if (lv2_midi_is_voice_message(msg)) {
+		unsafe{
+			return LV2_Midi_Message_Type::from_int((*msg) & 0xF0);
+		}
+	// } else if (lv2_midi_is_system_message(msg)) {
+	// 	return (LV2_Midi_Message_Type)msg[0];
+	} else {
+		return LV2_Midi_Message_Type::LV2_MIDI_MSG_INVALID;
+	}
+}
+
+pub enum LV2_Midi_Message_Type {
+	LV2_MIDI_MSG_INVALID          = 0,    // Invalid Message
+	LV2_MIDI_MSG_NOTE_OFF         = 0x80, // Note Off
+	LV2_MIDI_MSG_NOTE_ON          = 0x90,  // Note On
+	LV2_MIDI_MSG_NOT_IMPLEMENTED  = 9999999999999  //
+}
+
+// Unnecessary?
+impl LV2_Midi_Message_Type {
+    fn from_int(x: u8) -> LV2_Midi_Message_Type {
+        match x {
+            0 => LV2_Midi_Message_Type::LV2_MIDI_MSG_INVALID,
+            0x80 => LV2_Midi_Message_Type::LV2_MIDI_MSG_NOTE_OFF,
+			0x90 => LV2_Midi_Message_Type::LV2_MIDI_MSG_NOTE_ON,
+            _ => LV2_Midi_Message_Type::LV2_MIDI_MSG_NOT_IMPLEMENTED
+        }
+    }
 }
