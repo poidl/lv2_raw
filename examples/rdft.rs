70526a2e926d5460618f7ff4399e56e8b6f1f761
poidl <hoitaus@gmail.com>
2015-12-11 16:15:17 +1100

    delete doc. contains to much copied stuff

--- examples/rdft.rs
+++ examples/rdft.rs
@@ -3,29 +3,27 @@ extern crate yassy;
 extern crate rgsl;
 extern crate gnuplot;
 
-// use std::ops::Deref;
-// use std::ops::DerefMut;
+// use std::ops::{Deref, DerefMut};
 
 use yassy::utils;
 
 fn main() {
     let pi = std::f64::consts::PI;
-    let n = 2u32.pow(12) as usize;
+    let n = 2u32.pow(5) as usize;
     let x = utils::linspace_heapslice(-1f64, 1f64, n);
     let x = x*2f64*pi;
     let mut y = x.sinc();
 
-    for ii in x.iter() {
-        println!("ii: {}",ii)
-    }
+    // for ii in x.iter() {
+    //     println!("ii: {}",ii)
+    // }
     // let z = y.copy();
     let l = y.len();
-    println!("l: {}", l);
-    let val = rgsl::fft::real_radix2::transform(&mut *y,1,l);
-    let xx = utils::linspace_heapslice(0f64, (n/2+1) as f64, n/2+1);
+    println!("length: {}", l);
+    let val = rgsl::fft::real_radix2::transform(&mut y,1,l);
+    // let xx = utils::linspace_heapslice(0f64, (n/2+1) as f64, n/2+1);
 
-TODO: *) understand copy and clone
-    *) auto deref instead of &mut *y
+// TODO: *) understand copy and clone
 
     let mut fg = gnuplot::Figure::new();
     fg.set_terminal("svg","./examples/hoit.svg");
