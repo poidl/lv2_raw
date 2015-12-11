51f328acbb897478f91435ccf69801f7f3aa1c03
poidl <hoitaus@gmail.com>
2015-12-11 17:28:18 +1100

    tinker with fft

--- examples/rdft.rs
+++ examples/rdft.rs
@@ -9,10 +9,12 @@ use yassy::utils;
 
 fn main() {
     let pi = std::f64::consts::PI;
-    let n = 2u32.pow(5) as usize;
+    let n = 2u32.pow(8) as usize;
     let x = utils::linspace_heapslice(-1f64, 1f64, n);
     let x = x*2f64*pi;
-    let mut y = x.sinc();
+    let xt = utils::linspace_heapslice(-1f64, 1f64, n);
+    let fc = (n) as f64;
+    let mut y = (xt*fc).sinc();
 
     // for ii in x.iter() {
     //     println!("ii: {}",ii)
@@ -24,13 +26,23 @@ fn main() {
     // let xx = utils::linspace_heapslice(0f64, (n/2+1) as f64, n/2+1);
 
 // TODO: *) understand copy and clone
-
+    // TODO inefficient:
+    let mut mag = utils::linspace_heapslice(-1f64, 1f64, n/2 +1);
+    for ii in 0..l/2 {
+        mag[ii]=(y[ii].powf(2f64)+y[l-1-ii].powf(2f64)).sqrt();
+    }
     let mut fg = gnuplot::Figure::new();
     fg.set_terminal("svg","./examples/hoit.svg");
     fg.axes2d()
     .lines(x.iter(), y.iter(), &[]);
     fg.show();
 
+    let x = utils::linspace_heapslice(0f64, (n/2) as f64, n/2+1);
+    let mut fg = gnuplot::Figure::new();
+    fg.set_terminal("svg","./examples/hoit2.svg");
+    fg.axes2d()
+    .lines(x.iter(), mag.iter(), &[]);
+    fg.show();
     // let mut fg = gnuplot::Figure::new();
     // fg.set_terminal("svg","./examples/hoit.svg");
     // fg.axes2d()
