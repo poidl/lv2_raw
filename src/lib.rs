6bb13881911f72f842937c2f96a5238ae4de462e
poidl <hoitaus@gmail.com>
2016-07-29 18:15:50 +1000

    rm comment

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,9 @@ pub mod atom;
 pub mod ui;
 pub mod urid;
 pub mod myutils;
-// pub mod midi;
 
 pub use core::*;
 pub use atom::*;
 pub use ui::*;
 pub use urid::*;
 pub use myutils::*;
-// pub use midi::*;
