0bf7806da84e47c863e5e2fbf253d0cb018ed98a
poidl <hoitaus@gmail.com>
2016-07-30 09:10:04 +1000

    fix subtree_branch.sh

--- scripts/subtree_branch.sh
+++ scripts/subtree_branch.sh
@@ -11,6 +11,8 @@ git checkout -b $1 $1/master
 mkdir scripts
 echo "git merge --squash -s subtree master" >> scripts/merge_master.sh
 chmod u+x scripts/merge_master.sh
+git add .
+git commit -m "add script to merge changes on master"
 
 # switch back to master and copy repo into subfolder
 git checkout master
