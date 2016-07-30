b4749b880d7f7a1f08c36bd83f832c2fb49c16b8
poidl <hoitaus@gmail.com>
2016-07-30 13:11:44 +1000

    update subtree_branch.sh

--- scripts/subtree_branch.sh
+++ scripts/subtree_branch.sh
@@ -11,16 +11,16 @@ git remote add $1 git@github.com:poidl/$1.git
 git fetch $1
 git checkout -b $1 $1/master
 
-# make script in branch to merge changes on master
-mkdir scripts
-echo "git merge --squash -s subtree master" >> scripts/merge_master.sh
-chmod u+x scripts/merge_master.sh
-git add .
-git commit -m "add script to merge changes on master"
+# # make script in branch to merge changes on master
+# mkdir scripts
+# echo "git merge --squash -s subtree master" >> scripts/merge_master.sh
+# chmod u+x scripts/merge_master.sh
+# git add .
+# git commit -m "add script to merge changes on master"
 
 # switch back to master and copy repo into subfolder
 git checkout master
 # is this necessary?
-#git merge -s ours --no-commit  $1/master
+it merge -s ours --no-commit  $1/master
 git read-tree --prefix=$1/ -u $1/master
 git commit -m "Subtree merged in "$1
