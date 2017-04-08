f6944210ceca65d325931f5e964baff419e5a424
Stefan <hoitaus@gmail.com>
2017-04-08 10:52:01 +1000

    copyright, doc

--- src/lv2utils.rs
+++ src/lv2utils.rs
@@ -1,3 +1,23 @@
+// Copyright 2017 Michael Oswald
+
+// Documentation copied from http://lv2plug.in/ns/lv2core/lv2_util.h
+
+// Copyright text of the original C file:
+
+// Copyright 2016 David Robillard <http://drobilla.net>
+
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+
+// THIS SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 
 use libc::{c_char, c_void};
 use core::*;
@@ -11,8 +31,10 @@ use std::ffi::*;
    only useful for features with data, and can not detect features that are
    present but have NULL data.
 */
-pub unsafe fn lv2_features_data(features: *const *const LV2Feature, curi: *const c_char) -> *mut c_void {
-    
+pub unsafe fn lv2_features_data(features: *const *const LV2Feature,
+                                curi: *const c_char)
+                                -> *mut c_void {
+
     if !features.is_null() {
         let mut feature = *features;
         let nul = 0 as *const LV2Feature;
@@ -28,7 +50,7 @@ pub unsafe fn lv2_features_data(features: *const *const LV2Feature, curi: *const
             feature = *features.offset(i);
             i += 1;
         }
-        
+
     }
     0 as *mut c_void
 }
@@ -37,10 +59,35 @@ pub unsafe fn lv2_features_data(features: *const *const LV2Feature, curi: *const
 pub struct FeatureHelper {
     urid: *const c_char,
     data: *mut *mut c_void,
-    required: bool
+    required: bool,
 }
 
-pub unsafe fn lv2_features_query(features: *const *const LV2Feature, query: &[FeatureHelper]) -> *const c_char {
+
+/**
+   Query a features array.
+
+   This function allows getting several features in one call, and detect
+   missing required features, with the same caveat of lv2_features_data().
+
+   The arguments should be a series of const char* uri, void** data, bool
+   required, terminated by a NULL URI.  The data pointers MUST be initialized
+   to NULL.  For example:
+
+   @code
+   LV2_URID_Log* log = NULL;
+   LV2_URID_Map* map = NULL;
+   const char* missing = lv2_features_query(
+        features,
+        LV2_LOG__log,  &log, false,
+        LV2_URID__map, &map, true,
+        NULL);
+   @endcode
+
+   @return NULL on success, otherwise the URI of this missing feature.
+*/
+pub unsafe fn lv2_features_query(features: *const *const LV2Feature,
+                                 query: &[FeatureHelper])
+                                 -> *const c_char {
 
     for it in query {
         let mut data = it.data;
