0bde24fc8ad12030c5e8b1455515f91e9e161ba4
Stefan <hoitaus@gmail.com>
2017-04-28 18:00:04 +1000

    simplify and comment

--- tests/testutils.rs
+++ tests/testutils.rs
@@ -1,35 +1,39 @@
 extern crate libc;
 extern crate lv2_raw;
 
-use libc::*;
+// use libc::*;
 use std::mem;
 use lv2_raw::*;
+
+// Need to define constant for buffer allocation, which must be identical
+// to n
+const N: usize = 64;
 
 #[test]
 fn it_works() {
 
-    // how much memory must be allocated
-    // sequence of two events
-    // 1* size of LV2AtomSequence (includes header and body)
+    // Construct a sequence of two events by hand:
+
+    // How much memory must be allocated?
+    // 1 * (size of LV2AtomSequence)
     // 2 * (size of event1)
-    // 2 * 4 for pad
-    // 2 * 4 for u32 data
-    const N: usize = 64;
+    // 2 * 8 for one u64 data per atom
+
     let s_seq = mem::size_of::<LV2AtomSequence>() as isize;
     let s_ev = mem::size_of::<LV2AtomEvent>() as isize;
-    let n = s_seq + 2*s_ev + 2*4 + 2*4;
+    let s_atom = 8 as isize;
+    let n = s_seq + 2*s_ev + 2*s_atom;
     if n != N as isize {
         panic!("Need to adjust buffer size. Size is {}. Buffer is {}.",
                n,
                N)
     }
 
-    let s_atom = mem::size_of::<LV2Atom>() as isize;
-    println!("**************size ATOM: {}", s_atom);
-    println!("**************size EVENT: {}", s_ev);
+    let s_atom_header = mem::size_of::<LV2Atom>() as isize;
+
     let atom = LV2Atom {
         // Size in bytes, not including type and size.
-        size: N as u32 - s_atom as u32,
+        size: N as u32 - s_atom_header as u32,
         // Type of this atom (mapped URI).
         mytype: 0,
     };
@@ -48,63 +52,73 @@ fn it_works() {
         body: seqbody,
     };
 
-    /// ////////////////////////////////////
+    ////////////////////////////////////////
     let atom_ev1 = LV2Atom {
-        size: 8,
-        mytype: 7,
+        size: s_atom as u32,
+        mytype: 7, // some random type
     };
     let atom_ev2 = LV2Atom {
-        size: 8,
-        mytype: 8,
+        size: s_atom as u32,
+        mytype: 8, 
     };
     let event1 = LV2AtomEvent {
-        time_in_frames: 33333i64,
+        time_in_frames: 33333i64, // some random timestamp
         body: atom_ev1,
     };
     let event2 = LV2AtomEvent {
         time_in_frames: 44444i64,
         body: atom_ev2,
     };
-    // event pad  size is 64 bits. use 32 here and pad with an u16
-    // then we have 16(that's s_ev)+16+32 = 64
-    // TODO: the padding is not necessary for testing
-    let atomdata1 = 11u32;
-    let atomdata2 = 22u32;
-    let pad = 0u32;
+    // event pad size is 64 bits, using u64 no is padding necessary
+    let atomdata1 = 11u64; // some random data
+    let atomdata2 = 22u64;
 
-
-    let mut buf = [1u8; N];
+    let buf = [1u8; N];
 
     unsafe {
-        let p1 = &mut buf[0] as *mut u8 as *mut libc::c_void;
-        let p2 = &sequence as *const LV2AtomSequence as *const libc::c_void;
-        libc::memcpy(p1, p2, s_seq as usize);
 
+        let mut state = State{buf: buf, current: 0};
 
-        let p2 = &event1 as *const LV2AtomEvent as *const libc::c_void;
-        libc::memcpy(p1.offset(s_seq), p2, s_ev as usize);
-        let p2 = &pad as *const u32 as *const libc::c_void;
-        libc::memcpy(p1.offset(s_seq + s_ev), p2, 4);
-        let p2 = &atomdata1 as *const u32 as *const libc::c_void;
-        libc::memcpy(p1.offset(s_seq + s_ev + 4), p2, 4);
+        let p = &sequence as *const LV2AtomSequence as *const libc::c_void;
+        state.append(p, s_seq);
 
-        let p2 = &event2 as *const LV2AtomEvent as *const libc::c_void;
-        libc::memcpy(p1.offset(s_seq + s_ev + 4 + 4), p2, s_ev as usize);
-        let p2 = &pad as *const u32 as *const libc::c_void;
-        libc::memcpy(p1.offset(s_seq + 2*s_ev + 4 + 4), p2, 4);
-        let p2 = &atomdata2 as *const u32 as *const libc::c_void;
-        libc::memcpy(p1.offset(s_seq + 2*s_ev + 2*4 + 4), p2, 4);
+        // Event 1 
+        let p = &event1 as *const LV2AtomEvent as *const libc::c_void;
+        state.append(p, s_ev);
+        let p = &atomdata1 as *const u64 as *const libc::c_void;
+        state.append(p, s_atom);
 
+        // Event 2
+        let p = &event2 as *const LV2AtomEvent as *const libc::c_void;
+        state.append(p, s_ev);
+        let p = &atomdata2 as *const u64 as *const libc::c_void;
+        state.append(p, s_atom);
 
-        let seq = &buf[0] as *const u8 as *const LV2AtomSequence;
+        // let seq = &buf[0] as *const u8 as *const LV2AtomSequence;
+        let seq = &state.buf[0] as *const u8 as *const LV2AtomSequence;
         for ev in &*seq {
             println!{"*************TIME: {}", ev.time_in_frames}
             println!{"*************ATOM.MYTYPE: {}", ev.body.mytype}
             let atomptr = &ev.body as *const LV2Atom as *const u8;
-            let dataptr = atomptr.offset(s_atom+4);
-            let data = *(dataptr as *const u32);
+            let dataptr = atomptr.offset(s_atom_header);
+            let data = *(dataptr as *const u64);
             println!{"************ data: {}", data};
         }
     }
     assert_eq!(4, 4);
+}
+
+struct State {
+    buf: [u8; N],
+    current: isize
+}
+
+impl State {
+    fn append(&mut self, p: *const libc::c_void, size: isize) {
+        let p1 = &mut self.buf[0] as *mut u8 as *mut libc::c_void;
+        unsafe {
+            libc::memcpy(p1.offset(self.current), p, size as usize);
+        }
+        self.current = self.current + size;
+    }
 }
